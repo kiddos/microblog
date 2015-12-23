0b4995e12b8a7681b9e5494591ece836cb77b7d7
kiddos <josephyu831007@yahoo.com.tw>
2015-12-23 11:10:53 +0800

    code cleanup

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
 var settings = require('./settings');
-// var MongoStore = require('connect-mongo');
 var MongoStore = require('connect-mongo')(express);
-
 var app = module.exports = express.createServer();
-
-// Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -58,4 +50,6 @@ app.dynamicHelpers({
 });
 
 app.listen(settings.debugPort);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Express server listening on port %d in %s mode",
+            app.address().port, app.settings.env);
+
