010ca415991cc4c90072eb334c46d10d15b0a250
StoneLin0708 <jason840708@gmail.com>
2015-12-29 01:56:31 +0800

    a bug whan post image

--- models/post.js
+++ models/post.js
@@ -9,16 +9,22 @@ function Post(username, post, time) {
   } else {
     this.time = new Date();
   }
+  if (post.image) {
+    this.image = post.image;
+  }
 }
 module.exports = Post;
 
-Post.prototype.save = function save(callback) {
+Post.prototype.save = function save(newImage,callback) {
   // 存入 Mongodb 的文檔
   var post = {
     user: this.user,
     post: this.post,
     time: this.time,
+    image: this.image,
   };
+  if(newImage === null)
+    post.image = null;
   mongodb.open(function(err, db) {
     if (err) {
       return callback(err);
@@ -38,6 +44,18 @@ Post.prototype.save = function save(callback) {
       });
     });
   });
+  if(newImage === null){
+    // save the image data
+    var imagePath = __dirname + "/../public/img/posts/" + this.user + this.time + '.jpg';
+    fs.writeFile(imagePath, newImage, 'binary', function(err) {
+      if (err) {
+        console.log(err);
+        return callback(err);
+      }
+      console.log('new image saved for ' + this.user + this.time);
+      return callback(err, user);
+    });
+  }
 };
 
 Post.get = function get(username, callback) {
@@ -64,11 +82,45 @@ Post.get = function get(username, callback) {
         // 封裝 posts 爲 Post 對象
         var posts = [];
         docs.forEach(function(doc, index) {
-          var post = new Post(doc.user, doc.post, doc.time);
+          var post = new Post(doc.user, doc.post, doc.time, doc.image);
           posts.push(post);
         });
         callback(null, posts);
       });
     });
+  });
+};
+
+Post.prototype.saveImage = function(newImage, callback) {
+  var post = {
+    user: this.user,
+    post: this.post,
+    time: this.time,
+    image: '/img/posts/' + this.user + this.time + '.jpg',
+  };
+  // open mongodb and update the user
+  mongodb.open(function(err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection(settings.postsCollection, function(err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      collection.update({name: post.user,post: post.post}, post, function(err, results) {
+        mongodb.close();
+      });
+    });
+  });
+  // save the image data
+  var imagePath = __dirname + "/../public/img/posts/" + this.user + this.time + '.jpg';
+  fs.writeFile(imagePath, newImage, 'binary', function(err) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    console.log('new image saved for ' + this.user + this.time);
+    return callback(err, user);
   });
 };
